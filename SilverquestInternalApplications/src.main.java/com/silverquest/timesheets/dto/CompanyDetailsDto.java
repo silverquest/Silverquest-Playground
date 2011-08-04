afc7c17dc085b351473fd6faef42470726431e68
Lisa Johnston <nicshel2@gmail.com>
2011-08-05 09:25:52 +1000

    New files

---
+++ SilverquestInternalApplications/src.main.java/com/silverquest/timesheets/dto/CompanyDetailsDto.java
@@ -0,0 +1,21 @@
+package com.silverquest.timesheets.dto;
+
+public class CompanyDetailsDto {
+
+	private String id;
+	private String companyName;
+
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	
+}
---
+++ SilverquestInternalApplications/test.main.java/com/silverquest/timesheets/jdo/service/impl/CompanyJdoServiceImplTest.java
@@ -0,0 +1,64 @@
+package com.silverquest.timesheets.jdo.service.impl;
+
+
+import org.junit.Test;
+
+
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.silverquest.timesheets.dto.AddressDto;
+import com.silverquest.timesheets.dto.CompanyDto;
+import com.silverquest.timesheets.dto.CompanyType;
+
+public class CompanyJdoServiceImplTest extends TestCase {
+
+	
+    private final LocalServiceTestHelper helper =
+            new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
+
+
+    @Before
+    public void setUp() {
+        helper.setUp();
+    }
+
+    @After
+    public void tearDown() {
+        helper.tearDown();
+    }
+    
+    @Test
+    public void testSave(){
+    	
+    	try{
+    	
+	    	CompanyDto dto = new CompanyDto();
+	    	dto.setCompanyName("Lisa's Company");
+	    	dto.setType(CompanyType.CLIENT_COMPANY);
+	    	AddressDto address = new AddressDto();;
+	    	address.setAddressLine1("72 Avondale Park");
+	    	dto.setAddress(address);
+	    	
+	    	CompanyJdoServiceImpl service = new CompanyJdoServiceImpl();
+	    	
+	    	dto = service.save(dto);
+	    	
+	    	System.out.println(dto.getId());
+	    	System.out.println("xxx");
+    	}
+    	catch(Exception e){
+    		e.printStackTrace();
+    	}
+    	
+    	
+    	
+    }
+
+
+}
+
+
