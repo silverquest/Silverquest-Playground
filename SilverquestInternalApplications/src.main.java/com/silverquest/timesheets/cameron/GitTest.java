320512c1b5110dd0932f1a0b3375f8b6aec34047
Cameron Moses <Cameron.Moses@silverquest.com.au>
2011-08-09 13:45:26 +1000

    Test commit of a package & class

---
+++ SilverquestInternalApplications/src.main.java/com/silverquest/timesheets/cameron/GitTest.java
@@ -0,0 +1,13 @@
+package com.silverquest.timesheets.cameron;
+
+public class GitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
