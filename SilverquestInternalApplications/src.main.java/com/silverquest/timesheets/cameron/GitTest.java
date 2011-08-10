00460af37583225bc02b5a4ecf78cf555b49978a
Cameron Moses <cameron.moses@silverquest.com.au>
2011-08-10 11:12:48 +1000

    Test 11.12

--- SilverquestInternalApplications/src.main.java/com/silverquest/timesheets/cameron/GitTest.java
+++ SilverquestInternalApplications/src.main.java/com/silverquest/timesheets/cameron/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// another comment for testing new commit to origin and then upstream
 
 	}
 
